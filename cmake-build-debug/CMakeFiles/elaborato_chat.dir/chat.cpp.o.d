a00cc9656189e20d6f51c1b275498f3326f28c81
leoke <leoviti9@gmail.com>
2023-07-14 17:21:11 +0200

    codice base

---
+++ cmake-build-debug/CMakeFiles/elaborato_chat.dir/chat.cpp.o.d
@@ -0,0 +1,4 @@
+CMakeFiles/elaborato_chat.dir/chat.cpp.o: \
+ /mnt/c/Users/leoke/CLionProjects/elaborato_chat/chat.cpp \
+ /usr/include/stdc-predef.h \
+ /mnt/c/Users/leoke/CLionProjects/elaborato_chat/chat.h
