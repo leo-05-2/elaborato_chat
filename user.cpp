03af4a9b7816afec3da69429f906b2d9efb48215
leoke <leoviti9@gmail.com>
2023-07-19 11:28:42 +0200

    added user method

--- user.cpp
+++ user.cpp
@@ -4,8 +4,14 @@
 
 #include "user.h"
 #include "message.h"
-void user::send_message(const std::string &message_text, user &receiver) {
-    Message message(message_text,get_user_id(), receiver.get_user_id());
-
-
+bool user::send_message(const std::string &message_text, user &receiver) {
+    if(receiver==*this||message_text.empty()) {
+        return false;
+    }
+    else{
+        Message message(message_text, this->user_id, receiver.get_user_id());
+        receiver.add_message(message);
+        return true;
+    }
 }
+
--- user.h
+++ user.h
@@ -4,15 +4,18 @@
 
 #ifndef ELABORATO_CHAT_USER_H
 #define ELABORATO_CHAT_USER_H
+
+#include <vector>
 #include "message.h"
 
 
 class user {
 public:
-    user(std::string &user_id, std::string &user_name, std::string &user_surname):
+    user(std::string &user_id, std::string &user_name,std::string &user_surname):
     user_id(user_id), user_name(user_name), user_surname(user_surname){};
+    user(std::string id):user_id(id),user_name("default name"), user_surname("default cognome" ){};
 
-    void send_message(const std::string &message_text, user &receiver);
+    bool send_message(const std::string &message_text, user &receiver);
     const std::string & get_user_id()const{
         return user_id;
     };
@@ -22,10 +25,22 @@ public:
     std::string get_user_surname(){
         return user_surname;
     };
+    std::vector<Message> &get_messages(){
+        return messages;
+    }
+    void add_message(Message &message){
+        messages.push_back(message);
+    }
+
+    bool operator==(const user &rs) const {
+        return user_id == rs.user_id;
+    }
+
 private:
     std::string user_id;
     std::string user_name;
     std::string user_surname;
+    std::vector<Message> messages;
 
 
 
